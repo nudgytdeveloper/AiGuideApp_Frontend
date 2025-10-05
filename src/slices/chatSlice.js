12a501e6d2e4bf96af4e01fa9bf38de8d9648c69
Chan Thai Thong <thongz0819@live.com>
2025-10-05 21:17:09 +0800

    - add chat slice

---
+++ src/slices/chatSlice.js
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit"
+import { fromJS } from "immutable"
+
+const initialState = fromJS({
+  isProcessing: false,
+  isListening: false,
+  conversationHistory: [
+    {
+      role: "system",
+      content:
+        "You are the AI Assistant for a Singaporean company called Nudgyt. You are nice and friendly.",
+    },
+  ],
+  lastInteractionTime: Date.now(),
+})
+
+const chatSlice = createSlice({
+  name: "chat",
+  initialState,
+  reducers: {
+    setIsProcessing: (state, action) => {
+      return state.merge(
+        fromJS({
+          isProcessing: action.payload,
+        })
+      )
+    },
+    setIsListening: (state, action) => {
+      return state.merge(
+        fromJS({
+          isListening: action.payload,
+        })
+      )
+    },
+    setConversationHistory: (state, action) => {
+      return state.merge(
+        fromJS({
+          conversationHistory: action.payload,
+        })
+      )
+    },
+  },
+})
+
+export const { setIsProcessing, setIsListening, setConversationHistory } =
+  chatSlice.actions
+
+export default chatSlice.reducer
