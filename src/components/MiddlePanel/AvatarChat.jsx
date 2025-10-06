a4dc7695e8c7d0e0b9b7f1f6c9b476ceba3183c1
Chan Thai Thong <thongz0819@live.com>
2025-10-06 12:18:39 +0800

    - revert directory

--- src/components/MiddlePanel/AvatarChat.jsx
+++ src/components/MiddlePanel/AvatarChat.jsx
@@ -93,7 +93,7 @@ const AvatarChat = () => {
     const loader = new GLTFLoader()
 
     loader.load(
-      "SCB_female.glb",
+      "src/SCB_female.glb",
       (gltf) => {
         const model = gltf.scene
         model.position.y = -0.3 // Move the model down
@@ -112,7 +112,7 @@ const AvatarChat = () => {
 
         // Load idle animation
         loader.load(
-          "idle.glb",
+          "src/idle.glb",
           (animGltf) => {
             if (animGltf.animations && animGltf.animations.length > 0) {
               const clip = filterAnimation(animGltf.animations[0])
