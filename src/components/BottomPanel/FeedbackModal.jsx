3248f78a4c21355f87b0e2e2aec526f801f7d21d
kathleennudgyt <kathleen.yu@nudgyt.com>
2025-12-02 05:45:48 +0800

    Added Google Form iFrame Integration as Feedback Modal

---
+++ src/components/BottomPanel/FeedbackModal.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './FeedbackModal.css';
+
+const FeedbackModal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
+  return (
+    <>
+      {/* The clickable link/button */}
+      <button className="feedback-button" onClick={openModal}>
+        Give Feedback
+      </button>
+
+      {/* The modal overlay with the embedded form */}
+      {isOpen && (
+        <div className="feedback-modal-overlay" onClick={closeModal}>
+          <div className="feedback-modal-content" onClick={(e) => e.stopPropagation()}>
+            {/* Close button */}
+            <button className="feedback-close-button" onClick={closeModal}>
+              ×
+            </button>
+            
+            {/* Embedded Google Form */}
+            <iframe
+              src="https://docs.google.com/forms/d/e/1FAIpQLSev3x-THtvx_7Ys_QrIyKW2PaqoSNYkffaH63lLF2czCukIDA/viewform?embedded=true"
+              width="100%"
+              height="100%"
+              frameBorder="0"
+              marginHeight="0"
+              marginWidth="0"
+              title="Feedback Form"
+            >
+              Loading…
+            </iframe>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default FeedbackModal;
