6073d7f7b0f9ebbee083c133a51aa496e137333f
kathleennudgyt <kathleen.yu@nudgyt.com>
2025-12-02 06:58:17 +0800

    Update FeedbackModal.jsx

--- src/components/BottomPanel/FeedbackModal.jsx
+++ src/components/BottomPanel/FeedbackModal.jsx
@@ -8,32 +8,41 @@ const FeedbackModal = () => {
 
   return (
     <>
-      {/* The clickable link/button */}
       <button className="feedback-button" onClick={openModal}>
         Give Feedback
       </button>
 
-      {/* The modal overlay with the embedded form */}
       {isOpen && (
         <div className="feedback-modal-overlay" onClick={closeModal}>
           <div className="feedback-modal-content" onClick={(e) => e.stopPropagation()}>
-            {/* Close button */}
             <button className="feedback-close-button" onClick={closeModal}>
               ×
             </button>
             
-            {/* Embedded Google Form */}
-            <iframe
-              src="https://docs.google.com/forms/d/e/1FAIpQLSev3x-THtvx_7Ys_QrIyKW2PaqoSNYkffaH63lLF2czCukIDA/viewform?embedded=true"
-              width="100%"
-              height="2000"
-              frameBorder="0"
-              marginHeight="0"
-              marginWidth="0"
-              title="Feedback Form"
-            >
-              Loading…
-            </iframe>
+            <div style={{ 
+              width: '100%', 
+              height: '1600px', 
+              overflow: 'hidden',
+              background: 'white'
+            }}>
+              <iframe
+                src="https://docs.google.com/forms/d/e/1FAIpQLSev3x-THtvx_7Ys_QrIyKW2PaqoSNYkffaH63lLF2czCukIDA/viewform?embedded=true"
+                width="640"
+                height="1600"
+                frameBorder="0"
+                marginHeight="0"
+                marginWidth="0"
+                title="Feedback Form"
+                style={{
+                  border: 'none',
+                  width: '100%',
+                  height: '1600px',
+                  display: 'block'
+                }}
+              >
+                Loading…
+              </iframe>
+            </div>
           </div>
         </div>
       )}
